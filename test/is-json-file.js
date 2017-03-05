11ceaf1c8d570b98b496f69dcd83d99c877fda90
James Benner <james.benner@gmail.com>
2017-03-05 11:26:14 -0500

    Expanding code coverage.

--- test/is-json-file.js
+++ test/is-json-file.js
@@ -7,4 +7,16 @@ describe('is-json-file', () => {
     it('is a function', () => {
         expect(isJsonFile).to.be.a('function');
     });
+
+    it('returns `true` for a JSON file', () => {
+        let filename = 'taco-tuesday.json';
+
+        expect(isJsonFile(filename)).to.equal(true);
+    });
+
+    it('returns `false` for a txt file', () => {
+        let filename = 'laser-giraffe.txt';
+
+        expect(isJsonFile(filename)).to.equal(false);
+    });
 });
